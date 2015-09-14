0bbc07a44fd037872c12c66633d60c9f27245b38
Ed Hagen <ehagen@gmail.com>
2015-09-14 09:26:32 -0700

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,20 @@
+#' Coding of variables on all suicide reports from the probability sample.
+#'
+#' Coding of 23 variables related to the inclusive fitness and bargaining models of suicidal behavior.
+#' 
+#' Kristen Syme, Zach Garfield and Ed Hagen
+#'
+"coding"
+
+#' CDC data on suicide attempt and mortality rates
+#'
+#' United States 2001-2011
+#'
+"cdc"
+
+#' Cultures from the HRAF Probability Sample with information on suicide
+#' 
+#' Contains information on cultures in the HRAF probability sample, including
+#' information from matching SCCS cultures.
+#'
+"cultures"
