983e1cc1e2d2d8973ee4c6fcc45b32b0049521c7
Kristen Syme <klsyme@gmail.com>
2016-01-08 13:23:25 -0800

    clean up new data

--- hrafsuicidedata.R
+++ hrafsuicidedata.R
@@ -4,6 +4,10 @@ library(hrafsuicidedata)
 apology$transgression[apology$id == 446] = '0'
 apology$transgression[apology$id == 766] = '0'
 
+#Clean up new data
+d$unjustly_accused_punished[d$unjustly_accused_punished=='not_applicable'] = '0'
+d$unjustly_accused_punished <- as.numeric(d$unjustly_accused_punished)
+
 ##Remove duplicates and blank data
 apology <- apology[apology$id != 634,]
 apology = apology[apology$id != 1788,]
@@ -21,3 +25,8 @@ d$culture.y <- NULL
 d$extract.y <- NULL
 d$transgression.y <- NULL
 
+#Heatmap
+aheatmap(d[,c(7:13, 15)], hclustfun = 'ward')
+library(pvclust)
+m<-pvclust(d[,c(7:13, 15)])
+plot(m)
