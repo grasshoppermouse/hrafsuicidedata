93f594de8f99b5bc73c55683b31a5190720a2be7
Kristen Syme <klsyme@gmail.com>
2016-01-08 01:18:59 -0800

    Merge datasets

--- hrafsuicidedata.R
+++ hrafsuicidedata.R
@@ -4,11 +4,20 @@ library(hrafsuicidedata)
 apology$transgression[apology$id == 446] = '0'
 apology$transgression[apology$id == 766] = '0'
 
-##Remove duplicates
+##Remove duplicates and blank data
 apology <- apology[apology$id != 634,]
 apology = apology[apology$id != 1788,]
 apology = apology[apology$id != 1894,]
 apology = apology[apology$id != 1903,]
-apology = apology[apology$id != g,]
+apology = apology[apology$id != 'g',]
 
-##Need to remove 1905, 1906, 1908 mistakenly duplicated
+#Remove 1909 from apology
+apology <- apology[apology$id != 1909,]
+
+#Merge apology and transgression_data in d
+d$content_type.y <- NULL
+d$coder.y <- NULL
+d$culture.y <- NULL
+d$extract.y <- NULL
+d$transgression.y <- NULL
+
